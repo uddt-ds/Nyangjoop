330ba3c9e1e1f3369f9d925205c5b5302ee5808d
우다당탕 개발자 <uddt_ds@kakao.com>
2025-09-26 17:40:45 +0900

    [Feat] BaseViewController 구현

---
+++ Nyangjoop/Base/BaseViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BaseViewController.swift
+//  Nyangjoop
+//
+//  Created by Lee on 9/26/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func configureHierarchy() { }
+
+    func configureLayout() { }
+
+    func configureView() { }
+}
