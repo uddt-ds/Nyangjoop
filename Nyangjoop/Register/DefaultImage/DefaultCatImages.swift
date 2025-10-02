0f18ddf89ea264862be07d7b952499d7359ed046
우다당탕 개발자 <uddt_ds@kakao.com>
2025-10-02 15:45:54 +0900

    [Design] 고양이 이미지 에셋 등록

--- Nyangjoop/Register/DefaultImage/DefaultCatImages.swift
+++ Nyangjoop/Register/DefaultImage/DefaultCatImages.swift
@@ -9,12 +9,20 @@ import Foundation
 
 struct DefaultCatImages {
     static let imageNames = [
-        "black1_x1", "black1_x1", "black1_x1",
-        "black1_x1", "black1_x1", "black1_x1",
-        "black1_x1", "black1_x1", "black1_x1",
-        "black1_x1", "black1_x1", "black1_x1",
-        "black1_x1", "black1_x1", "black1_x1",
-        "black1_x1", "black1_x1", "black1_x1",
-        "black1_x1", "black1_x1", "black1_x1"
+        // Beige 계열 (6개)
+        "beige1", "beige2", "beige3",
+        "beige4", "beige5", "beige6",
+        
+        // Brown 계열 (6개)
+        "brown1", "brown2", "brown3",
+        "brown4", "brown5", "brown6",
+        
+        // Gray 계열 (6개)
+        "gray1", "gray2", "gray3",
+        "gray4", "gray5", "gray6",
+        
+        // White 계열 (6개)
+        "white1", "white2", "white3",
+        "white4", "white5", "white6"
     ]
 }
